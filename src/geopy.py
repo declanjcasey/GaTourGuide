d870d580dab9b8a5645078a21378fc1ea3482a5f
Declan Casey <declancasey@declans-air-2.myfiosgateway.com>
2021-01-06 13:02:30 -0500

    change file structure

--- src/geopy.py
+++ src/geopy.py
@@ -6,3 +6,6 @@ GEOLOCATOR = Nominatim(user_agent="GatourGuide")
 def calcCoordinates(self, location):
     coordinates = location.latitude, location.longitude
     return coordinates
+
+
+# Other functions to add: calcDistance
